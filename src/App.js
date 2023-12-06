17e6197a32b1340f43020da3ef9bbd498e42b2a1
jacekroszkowiak <jacekroszkowiakdev@gmail.com>
2023-12-06 17:03:46 +0100

    components created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import './App.css';
+import "./App.css";
+import ColorButton from "./components/ColorButton.component";
 
 function App() {
-  return (
-    <div className="App">
-      <hi1>Add your code here!</hi1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <ColorButton />
+        </div>
+    );
 }
 
 export default App;
